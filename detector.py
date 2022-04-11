fc6cb660539fb0edd06533a1aac94a40eb8ee13d
Kinan <kinansobih29@gmail.com>
2022-04-11 05:05:48 +0300

    Make detector.py as runnable script

--- detector.py
+++ detector.py
@@ -1,24 +1,28 @@
+import json
 from threading import Thread
 import pytesseract
 import socket
 from datetime import datetime
 import api
 
-class Detector:
-        def start_plate_detection():
-                pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-                api_result = api.ApiRequest.check_plate("car3.jpg");
-                plate_number = api_result.get('results')[0]['plate']
-                dt = datetime.now()
-                date_now = dt.strftime("%d/%m/%Y")
-                raw_data = {'date': date_now, 'plate_number': plate_number, 'url': 'http://www.google.com'}
+def openSocketWithFiras(data_to_send):
+        HOST = "192.168.1.17"  # The server's hostname or IP address
+        PORT = 5000  # The port used by the server
 
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((HOST, PORT))
+                print(bytes(data_to_send,encoding="utf-8"))
+                s.sendall(bytes(data_to_send,encoding="utf-8"))
+                data = s.recv(1024)
 
-                HOST = "192.168.1.17"  # The server's hostname or IP address
-                PORT = 5000  # The port used by the server
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                        s.connect((HOST, PORT))
-                        print(bytes(raw_data,encoding="utf-8"))
-                        s.sendall(bytes(raw_data,encoding="utf-8"))
-                        data = s.recv(1024)
+api_result = api.ApiRequest.check_plate("./violations/output1.jpg");
+if len(api_result.get('results')) > 0:
+        plate_number = api_result.get('results')[0]['plate']
+        dt = datetime.now()
+        date_now = dt.strftime("%d/%m/%Y")
+        raw_data = {'date': date_now, 'plate_number': plate_number, 'url': 'https://drive.google.com/file/d/1wqMM2ankspzy4_HCmrk4uqMJ8AJN1Koj/view?usp=sharing'}
+        raw_data = json.dumps(raw_data)
+
+        openSocketWithFiras(raw_data)
